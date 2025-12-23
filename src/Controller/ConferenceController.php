a7a5633a8fce07a3d568d7f72777e437ac50ab18
Julius Knows <juliusknows@MacBook-Air-Julius.local>
2025-12-23 15:03:46 +0300

    first commit

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,56 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-
 final class ConferenceController extends AbstractController
 {
- 
-    public function hello(Request $request): Response
+    public function index(): Response
     {
-        dump($request);
         
-        return new Response(<<<EOF
-            <html>
-                <body style="
-                    margin: 0;
-                    height: 100vh;
-                    background-image: url('/images/skyposter.jpg');
-                    background-size: cover;
-                    background-position: center;
-                    background-repeat: no-repeat;
-                ">
-                    <a href="/info">подробнее тут</a>
-                    <img src="/images/under-construction.gif" alt="Реконструкция" style="max-width:80%;height:auto;display:block;margin:50px auto;">
-                </body>
-            </html>
-            EOF
-        );
-    }
-    
-
-    public function bye(): Response
-    {
-        return new Response(<<<EOF
-            <html>
-                <body style="
-                    margin: 0;
-                    height: 100vh;
-                    background-image: url('/images/monky.jpg');
-                    background-size: cover;
-                    background-position: center;
-                    background-repeat: no-repeat;
-                ">
-                    Можешь в принципе
-                    <a href="/"> вернуться</a>
-                </body>
-            </html>
-            EOF
-        );
-                
+        return $this->render('conference/index.html.twig', [
+            'controller_name' => 'ConferenceController',
+        ]);
     }
 }
