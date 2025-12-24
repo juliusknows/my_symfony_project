07678393931048ed42d2e341066668b6ada25900
juliusknows <juliusknows@localhost>
2025-12-24 20:34:36 +0300

    тут все заменил, проверил, работает

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,9 +9,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
+    #[Route('/conference', name: 'app_conference')]
     public function index(): Response
     {
-        
+
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
